877644a5470d94552ae8326b63e10750a68d2800
Matthew Barnfield <m.barnfield@qmul.ac.uk>
2019-06-12 13:53:53 +0100

    more docs

--- R/cjbae_df.R
+++ R/cjbae_df.R
@@ -1,14 +1,49 @@
-cjbae_df <- function(brmsfit) {
+#' @rdname cjbae_df
+#' @title Tidy Bayesian AMCE dataframe
+#' @description Generate a tidy dataframe of Bayesian AMCEs from brmsfit
+#' @param data A tidy conjoint dataframe - the original dataset you used in the AMCE model.
+#' @param formula A standard formula of the form outcome ~ feature 1 + feature 2 ... + feature n - the same as the one plugged into the AMCE model.
+#' @param brmsfit The brmsfit object to convert.
+#' @param iter The number of iterations in the brms model. Defaults to 2000, the same as the default in amce_bae and cjbae.
+#' @return A dataframe of AMCEs. These take the form of samples from the posterior probability distribution and can be plotted as distributions, rather than point estimates.
+#' @details \code{cjbae_df()} takes an AMCE brmsfit object and creates a dataframe of AMCE parameter estimates. The other arguments are needed in addition to the brmsfit argument in order to create the feature variable in the output.
+#' @export
+#' @import tidyverse magrittr reshape2 stats
+#' @examples
+#' #' #' #load example dataset from {cregg} (Leeper 2019)
+#' library(cregg)
+#' data(taxes)
+#' # formula
+#' f <- chose_plan ~ taxrate1 + taxrate2 + taxrate3 + taxrate4 + taxrate5 + taxrate6 + taxrev
+#' # prior - minimally informative
+#' prior <- c(set_prior("normal(0, .2)", class = "Intercept"),
+#' set_prior("normal(0, .2)", class = "b"),
+#' set_prior("exponential(10)", class = "sd"),
+#' set_prior("exponential(10)", class = "sigma"))
+#' # run generic function with MM and save specified, gives MMs as output and saves brmsfit to
+#' # working directory - this will take a while
+#' bae <- cjbae(data = taxes, formula = f, id = ID, estimate = "mm", prior = prior, save_amce = TRUE)
+#' # AMCE brmsfit object can be loaded from working directory using readRDS()
+#' # then converted into proper cjbae output
+#' cjbae_df(taxes, f, baerms)
+#' # if the iter argument in cjbae() is altered from the default, the same is required in cjbae_df()
+#' bae <- cjbae(data = taxes, formula = f, id = ID, estimate = "mm", prior = prior, save_amce = TRUE, iter = 1e3)
+#' cjbae_df(taxes, f, baerms, iter = 1e3)
 
-  baemces <- posterior_samples(brmsfit, "^b") %>%
+cjbae_df <- function(data, formula, brmsfit, iter = 2e3) {
+
+  baemces <- posterior_samples(baerms, "^b") %>%
     data.frame() %>%
-    select(-"b_Intercept") %>%
+    dplyr::select(-"b_Intercept") %>%
     reshape2::melt() %>%
     mutate(variable = gsub(".*_",
                            "",
                            variable)) %>%
     rename(estimate = value) %>%
     rename(level = variable)
+
+  # predictors var to help generate feature var
+  predictors <- all.vars(stats::update(formula, 0 ~ .))
 
   # create feature variable - first have to work out no. of unique levels per feature
   features_df <- dplyr::select(data, one_of(predictors))
--- R/cjbae_formula.R
+++ R/cjbae_formula.R
@@ -1,12 +1,13 @@
 #' @rdname cjbae_formula
-#' @title Conjoint formula conversion for bayesian analysis
-#' @description Convert standard tidy conjoint function (data, formula, id) input into the input for a `brmsformula()`.
+#' @title Conjoint formula conversion for Bayesian analysis
+#' @description Convert standard tidy conjoint function (data, formula, id) input into the input for a `brms` model.
 #' @param data A tidy, long-format conjoint dataframe.
 #' @param formula A formula specifying an AMCE/MM model, to be converted for bayesian analysis. All variables should be factors.
 #' @param id A variable, within data, containing respondent IDs.
-#' @details \code{cjbae_formula} takes three arguments: `data`, `formula`, and `id`, and uses these to specify a formula to be passed to `brmsformula()`, to facilitate bayesian analysis of conjoint experiments otherwise analysed using `cregg`.
+#' @details \code{cjbae_formula} takes three arguments: `data`, `formula`, and `id`, and uses these to specify a formula to be passed to `brm()`, to facilitate bayesian analysis of conjoint experiments otherwise analysed using `cregg`. This function is integrated into other `cjbae()` functions and only necessary in isolation if you are directly using `brms()`.
 #' @export
-#' @examples 
+#' @import tidyverse stats
+#' @examples
 #' #load example data from cregg
 #' library(cregg)
 #' data(immigration)
@@ -15,32 +16,30 @@
 #' #or matched positionally
 #' cjbae_formula(immigration, ChosenImmigrant ~ Gender + Job, CaseID)
 #' #can pre-specify longer input formula
-#' f1 <- ChosenImmigrant ~ Gender + Education + LanguageSkills + 
-#' CountryOfOrigin + Job + JobExperience + JobPlans + 
+#' f1 <- ChosenImmigrant ~ Gender + Education + LanguageSkills +
+#' CountryOfOrigin + Job + JobExperience + JobPlans +
 #' ReasonForApplication + PriorEntry
 #' cjbae_formula(immigration, f1, CaseID)
 
 cjbae_formula <-
   function(data, formula, id) {
-    
-    library(tidyverse)
-    
+
     id_enq <- enquo(id)
-    
+
     #find lhs and rhs vars
     outcome <- all.vars(stats::update(formula, . ~ 0))
     if (!length(outcome) || outcome == ".") {
       stop("'formula' is missing a left-hand outcome variable")
     }
     predictors <- all.vars(stats::update(formula, 0 ~ .))
-    
+
     #collapse predictors in rhs string
-    predictors_string <- paste(predictors, collapse = " + ")  
-    
+    predictors_string <- paste(predictors, collapse = " + ")
+
     #get id variable from data
     id <- data %>%
       dplyr::select(!!id_enq)
-    
+
     #create
     bae_formula <- paste0(outcome,
                           " ~ 1 + ",
@@ -48,7 +47,7 @@ cjbae_formula <-
                           " + (1 | ",
                           colnames(id),
                           ")")
-    
+
     # Output as formula
     formula(bae_formula)
   }
